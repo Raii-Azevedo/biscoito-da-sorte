5a8e8f003711319a9c3855d61625a3fad3b7d66f
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-02-08 19:32:08 -0300

    fixing bugs

--- App.js
+++ App.js
@@ -146,7 +146,7 @@ class App extends Component {
 
               <Image source={this.state.img} style={styles.img} />
 
-              <Text style={[styles.textoCookie, { color: '#dd7b22', fontSize: 25, marginTop: 25 }]}>
+              <Text style={[styles.textoCookie, { color: '#dd7b22', fontSize: 20, marginTop: 20 }]}>
                 {this.state.textoCookie}
               </Text>
 
